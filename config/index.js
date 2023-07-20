d1b80cfab3925dc483ed0288eb8b5d3e989d5d15
= <=>
2023-07-20 16:12:09 +0500

    OK

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ require('dotenv/config');
 const {env} = process;
 
 const config = {
-    port: +env.port,
-    jwt_key: env.jwt_secret
+    port: 8080,
+    jwt_key: "env.jwt_secret"
 }
 
 module.exports = config;
